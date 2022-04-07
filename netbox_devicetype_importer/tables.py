a99b781fb7970b0b4527e46065775a276da53f2b
Юзефович Николай Игоревич <uni@skbkontur.ru>
2022-04-07 18:22:02 +0500

    nb 3.2 support;

--- netbox_devicetype_importer/tables.py
+++ netbox_devicetype_importer/tables.py
@@ -1,10 +1,15 @@
-from utilities.tables import BaseTable, ToggleColumn
+# for 3.2 support
+try:
+    from utilities.tables import BaseTable, ToggleColumn
+except ImportError:
+    from netbox.tables import BaseTable
+    from netbox.tables.columns import ToggleColumn
 
 from .models import MetaDeviceType
 
 
 class MetaDeviceTypeTable(BaseTable):
-    pk = ToggleColumn()
+    pk = ToggleColumn(visible=True)
     id = None
 
     def render_name(self, value):
